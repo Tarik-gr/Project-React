2a5e1c2f8c449feefb58d8a95fd38f514c311e6c
Tarik-Gr <tarik-gr@hotmail.fr>
2022-03-19 19:02:04 +0100

    minor modifications

--- src/Components/ErrorPage/index.js
+++ src/Components/ErrorPage/index.js
@@ -7,7 +7,7 @@ const ErrorPage = () => {
         <div style={{textAlign: 'center',}}>
             <h2>404</h2>
             <p>Cette page n'existe pas</p>
-            <img src={errorImg}/>
+            <img src={errorImg} alt="page404"/>
             <br />
             <Link to='/' className='btn btn-block btn-lg btn-primary'> Aller à la page d'accueil </Link>
         </div>
